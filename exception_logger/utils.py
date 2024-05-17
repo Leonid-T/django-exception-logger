360759763ac8f47bba0f4ff500b2b8552e0845b0
Leonid <titov281@yandex.ru>
2024-05-17 19:44:35 +0500

    fix - pretty_json

--- exception_logger/utils.py
+++ exception_logger/utils.py
@@ -7,6 +7,6 @@ def pretty_json(json_string):
     return mark_safe(
         "<style>div.readonly { width: 100%; overflow-wrap: break-word }</style>"
         '<div style="white-space: pre-wrap; "width: 100%; overflow-wrap: break-word">'
-        f'{"<br>".join(json.dumps(json_string, indent=4).split("\n"))}'
-        "</div>"
+        + "<br>".join(json.dumps(json_string, indent=4).split("\n"))
+        + "</div>"
     )
